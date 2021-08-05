360d09995f80084242422d4eda3b2958d86f32d4
DevFreak <hmengineer@outlook.com>
2021-08-05 19:21:13 +1000

    DB Comments

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -1,5 +1,8 @@
 const getDB = async (country) => {
   try {
+    // TODO:  El archivo de base de datos (temporal) usado a continuación, idealmente debera ser alojado en una base de datos real.
+    //        Se recomienda una base de datos no-relacional (MongoDB, MariaDB) dado que la naturaleza de la data utilizada para el
+    //        proceso usualmente no se relaciona entre sí.
     const db = require("../database.json");
 
     const data = db.data.filter((item) => item.name === country)[0];
